1124d96380b7946926a7459330a0f37e07ff1ebb
4ntim4tter <4ntim4tter@gmail.com>
2023-01-27 17:33:08 +0100

    new working template

---
+++ resources/views/components/top-bar.blade.php
@@ -0,0 +1,11 @@
+<div class="container-fluid">
+    <div class="row align-items-center bg-light px-lg-5">
+        <div class="d-flex">
+            <div class="bg-primary text-white text-center py-2" style="width: 100px;">Trending</div>
+        </div>
+        <div class="d-flex" style="margin-left: auto;">
+            <div class="bg-primary text-white text-center py-2" style="width: 100px;">Login</div>
+            <div class="bg-primary text-white text-center py-2" style="width: 100px;">Sign Up</div>
+        </div>
+    </div>
+</div>
