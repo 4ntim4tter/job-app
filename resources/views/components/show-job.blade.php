77d4f5c5b886f1f1539d14badee86734f52fdf9e
4ntim4tter <4ntim4tter@gmail.com>
2023-01-29 17:11:48 +0100

    added form to job applications

--- resources/views/components/show-job.blade.php
+++ resources/views/components/show-job.blade.php
@@ -1,3 +1,48 @@
+<x-layout title="Job"/>
 <div>
-    {{ $job->name }}
+    <div class="container-fluid py-3">
+        <div class="container">
+            <div class="bg-light py-2 px-4 mb-3">
+                <h4 class="m-1">Company {{ $job->name }} is looking for {{ $job->category }}</h4>
+            </div>
+            <div class="bg-light py-2 px-4 mb-3">
+                <b>Job description:</b><p>{{ $job->description }}</p>
+                <b>Job requirements:</b><p>{{ $job->requirements }}</p>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="contact-form bg-light mb-3" style="padding: 30px;">
+                        <div id="success"></div>
+                        <form name="sentMessage" id="contactForm" novalidate="novalidate">
+                            <div class="form-row">
+                                <div class="col-md-6">
+                                    <div class="control-group">
+                                        <input type="text" class="form-control p-4" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="control-group">
+                                        <input type="email" class="form-control p-4" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <input type="text" class="form-control p-4" id="subject" placeholder="Qualitfications" required="required" data-validation-required-message="Please enter a subject" />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="control-group">
+                                <textarea class="form-control" rows="4" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary font-weight-semi-bold px-4" style="height: 50px;" type="submit" id="sendMessageButton">Apply Now</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
