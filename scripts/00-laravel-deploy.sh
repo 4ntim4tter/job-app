e69cd655782874645610b731ee26272b34245e4b
4ntim4tter <4ntim4tter@gmail.com>
2023-02-22 22:17:54 +0100

    deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+composer dump-autoload
+composer install --no-dev #--working-dir=/var/www/html
+
 
 echo "Caching config..."
 php artisan config:cache
