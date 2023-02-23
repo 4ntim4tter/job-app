49f0300b8b33511abe23730895567b74d0ce405a
Anes <4ntim4tter@gmail.com>
2023-02-23 09:33:34 +0100

    _

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
+composer global require laravel/installer
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
