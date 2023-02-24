fceb90eb6b2e033a17b7d296d24ba87f924b411f
Anes <4ntim4tter@gmail.com>
2023-02-24 09:38:12 +0100

    what

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require laravel/installer
+composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
@@ -8,8 +8,6 @@ php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
-php artisan optimize
-php artisan storage:link
 
 echo "Running migrations..."
 php artisan migrate --force
